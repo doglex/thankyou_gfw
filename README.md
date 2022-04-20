7274b667159a86f2686bbf54ccf475fcaf736439
Dogless <awsxsa@hotmail.com>
2022-04-21 00:19:44 +0800

    stash

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # thankyou_gfw
 + 需要设置允许ip访问
+
+## todo 
+startup时创建进去ssh，
+然后用ssh执行
+可以直接安装sshkey的
+或者在网页端先创建好sshkey
+
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import argparse
 import requests
-from api_key import api_key
+from api_key import api_key, ssh_pub
 
 url_prefix = "https://api.vultr.com/v2"
 headers_api = {"Authorization": f"Bearer {api_key}"}
@@ -14,7 +14,7 @@ def get_credit():
     print(f"${credit}\t{email}\tlast_pay@{account['last_payment_date'].split('+')[0]}")
 
 
-if __name__ == '__main__':
+def run_by_shell():
     parser = argparse.ArgumentParser(prog="python main.py")
     parser.add_argument('other', metavar='', type=str, nargs='*', help='other args')
     parser.add_argument('-c', '--credit', dest="c", action="store_true", default=False, help="显示余额")
@@ -22,3 +22,40 @@ if __name__ == '__main__':
     # print(args)
     if args.c:
         get_credit()
+
+
+def list_ssh_keys():
+    ans = requests.get(f"{url_prefix}/ssh-keys", headers=headers_api).json()
+    print(ans)
+
+
+def create_ssh_key():
+    ans = requests.post(f"{url_prefix}/ssh-keys", headers=headers_api,
+                        json={"name": "thankyou_gfw", "ssh_key": ssh_pub}).json()
+    print(ans)
+
+
+def get_ssh_key(n=0):
+    ans = requests.get(f"{url_prefix}/ssh-keys", headers=headers_api).json()
+    key_n = ans["ssh_keys"][n]["id"]
+    print(f"ssh_key: {key_n}")
+    return key_n
+
+def list_regions():
+    ans = requests.get(f"{url_prefix}/regions", headers=headers_api,).json()
+    for region in sorted(ans["regions"], key=lambda x:x["country"]):
+        print(f"{region['id']}, {region['city']}, {region['country']}")
+
+def list_plans():
+    ans = requests.get(f"{url_prefix}/plans", headers=headers_api, ).json()
+    print(ans)
+
+
+
+
+
+
+if __name__ == '__main__':
+    # get_credit()
+    # get_ssh_key()
+    list_plans()
